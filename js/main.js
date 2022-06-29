9744ed70ed87b6d508cbadcb3670fd9cb3258d28
tztomer <mytztomer@gmail.com>
2022-06-29 15:13:07 +0300

    add footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import appHeader from "./cmps/app-header.cmp.js";
+import appHeader from './cmps/app-header.cmp.js';
 import { router } from './router.js';
-// import appFooter from "./cmps/book-footer.cmp.js";
+import appFooter from './cmps/app-footer.cmp.js';
 // import userMsg from './cmps/user-msg.cmp.js';
 
 const options = {
@@ -9,16 +9,16 @@ const options = {
               <app-header />
               <!-- <user-msg/> -->
               <router-view/>
-              <!-- <app-footer /> -->
+              <app-footer />
           </section>
       `,
   components: {
     //   userMsg,
-      appHeader,
-    //   appFooter,
+    appHeader,
+    appFooter,
   },
 };
 
 const app = Vue.createApp(options);
-app.use(router)
-app.mount("#app");
+app.use(router);
+app.mount('#app');
